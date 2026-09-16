2a0d1a7a59b0441464fa3bed79fdc5059ff61a6f
Christer <christer.sundgren@systemkonstruktion.se>
2026-09-16 13:57:29 +0200

    calendar view

---
+++ bookingapp/src/services/Todo.tsx
@@ -0,0 +1,30 @@
+/*
+View Register free, new page and form
+API update: Error when free owerlaps for asset
+Free start_time bigger or equal to create time.
+Free end_time bigger than start_time.
+*/
+/*
+Register location, new page and form
+API update: 
+*/
+/*
+Register asset on location, new page and form
+API update: 
+Max assets on location ?
+*/
+/*
+Delete booked time
+API update: 
+Logged in user and future booking ?
+*/
+/*
+Calendar view shows 2 free, table has one
+API update: 
+?
+*/
+/*
+Calendar view shows booked time repeated two times 
+API update: 
+?
+*/
